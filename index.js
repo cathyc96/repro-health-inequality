b0c6b93f2b0bb731ee591b21129e40ddbfb06f1d
Catherine Cowell <catherine.cowell@tufts.edu>
2016-12-11 22:03:07 -0500

    half changes

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get('/more', function(request, response) {
   response.render('pages/more');
 });
 
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
